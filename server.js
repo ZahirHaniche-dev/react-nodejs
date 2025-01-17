ce9c8b5c8730ac69569e28b5b06744a83899965f
Haniche Zahir <zahir.haniche@inetum.com>
2025-01-18 00:34:32 +0100

    Add Redirection request

--- server.js
+++ server.js
@@ -11,22 +11,24 @@ const app = express();
 
 const path = require('path');
 
+
+// Je dit à App Express que j'utilise du JSON
+app.use(express.json());
+
+app.use(express.static('client/build'));
+// 
+app.get('/api/youtube', (_, res) => {
+    res.send({
+        msg: "Hello YouTube !"
+    })
+});
+
 // Servir les fichiers statiques du dossier build
 app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 // Rediriger toutes les autres requêtes vers le fichier index.html
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-});
-
-
-// Je dit à App Express que j'utilise du JSON
-app.use(express.json());
-// 
-app.get('/api/youtube', (_, res) => {
-    res.send({
-        msg: "Hello YouTube !"
-    })
 });
 
 // Ecoute si on est bien sur le port 500, si oui on retourne le console.log
