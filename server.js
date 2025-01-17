e7124167e4f4027ad1c92403598a9dc98b921bac
Haniche Zahir <zahir.haniche@inetum.com>
2025-01-17 19:15:19 +0100

    Add express get and add request GET

--- server.js
+++ server.js
@@ -4,12 +4,21 @@ const express = require('express');
 require("dotenv").config();
 
 // Déclare le port 500 en variable
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7000;
 
 // Déclaration du module app express
 const app = express();
 
+// Je dit à App Express que j'utilise du JSON
+app.use(express.json());
+// 
+app.get('/api/youtube', (_, res) => {
+    res.send({
+        msg: "Hello YouTube !"
+    })
+});
+
 // Ecoute si on est bien sur le port 500, si oui on retourne le console.log
 app.listen(PORT, () => {
-    console.log(`Server is running on port : ${5000}`);
+    console.log(`Server is running on port : ${PORT}`);
 });
